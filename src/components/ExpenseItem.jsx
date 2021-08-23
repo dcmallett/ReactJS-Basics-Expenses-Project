1191501ff620adacfea7a235b4f4c35b79f2c5c2
dcmallett <dcmallett28@gmail.com>
2021-08-23 22:15:45 +0100

    Styled the ExpenseItem component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,15 @@
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
     return (
-        <div>
-            <h2>Expense Item</h2>
-        </div>
-    );
+			<div className="expense-item">
+				<div>23/08/2021</div>
+				<div className="expense-item__description">
+					<h2>Car Insurance</h2>
+				</div>
+				<div className="expense-item__price">$300.99</div>
+			</div>
+	)
 }
 
 export default ExpenseItem;

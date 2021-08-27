f0be5e4968e2d25ca84f6773b87276026a0561c8
dcmallett <dcmallett28@gmail.com>
2021-08-27 23:50:30 +0100

    Adding Form submission and two way binding.

--- src/components/NewExpense/NewExpenseForm.jsx
+++ src/components/NewExpense/NewExpenseForm.jsx
@@ -23,22 +23,42 @@ const NewExpenseForm = (props) => {
         setEnteredDate(event.target.value);
     }
 
+    const submitFormHandler = (event) => {
+        //prevents the browser default on form submit
+        event.preventDefault();
+        const expenseData = {
+            //left properties names can be anything.
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        //so becuase we are using the value prop on the inputs as two way binding. 
+        //we then call our propertiy names on that value field and then in our handler or where,
+        //we need to reset the fields we re-call the state updatingfunctions. I.E setEnteredTItle and assign it to an empty string.
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+        console.log(expenseData);
+    }
 
     return (
-        <form>
+        <form onSubmit={submitFormHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Title:</label>
-                    {/* onChange can trigger on every key stroke and use the same event on all input types */}
-                    <input type="text" onChange={titleChangeHandler} />
+                    {/* onChange can trigger on every key stroke and use the same event on all input types 
+                            two way binding: inputs we can pass a new value back into the input reset and change the input programatically
+                            using the value attribute set internal value prop and set it a new value;
+                    */}
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Amount:</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Date:</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                    <input type="date" min="2019-01-01" value={enteredDate} max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
@@ -49,3 +69,22 @@ const NewExpenseForm = (props) => {
 }
 
 export default NewExpenseForm;
+
+
+
+
+
+
+
+
+
+/**
+ * whenever you depend on state, and you update the previous state
+ * you should use an alternate updateState function i.e  
+ * 
+ * 
+ * function syntax when your state depends on the previous state.
+ *  setUserInput((prevState) => {
+ *  return {...prevState, enteredTitle: event.target.value}
+ * })
+ */
